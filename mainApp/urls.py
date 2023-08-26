08cdb2af27027a3bee4cd16557e5a7405857ed18
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-27 00:43:32 +0600

    Update urls.py

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('delete/', views.deleteData, name = 'deleteData'),
     path('city/', views.CityInfo, name = 'CityInfo'),
     path('delete1/', views.DeleteCity, name = 'DeleteCity'),
+    path('course/', views.CourseView, name = 'CourseView'),
+    path('course1/', views.course_list, name = 'course_list'),
+    path('student/', views.student_view, name = 'student_view'),
 ]
