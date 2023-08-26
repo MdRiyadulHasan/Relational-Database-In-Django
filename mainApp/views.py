c6a4b890c39d19057461aabe4655754d9ebc7afe
Md. Riyadul Hasan <riadulhasancse48@gmail.com>
2023-08-27 00:43:27 +0600

    Update views.py

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, Author, City, Country
+from .models import Book, Author, City, Country, CourseInfo, Student
 # Create your views here.
 def index_view(request):
     allData = Book.objects.all()
@@ -42,4 +42,52 @@ def DeleteCity(request):
     else:
         return HttpResponse("Not Deleted Country")
 
-    
+def CourseView(request):
+    allData =   CourseInfo.objects.all()
+    infoData = []
+    for data in allData:
+        placejson = {}
+        placejson['id'] = data.id
+        placejson['title'] = data.title
+
+        student_list =  []
+        for student in data.students.all():
+            student_list.append(student.name)
+        placejson['students'] = student_list
+
+        infoData.append(placejson)
+    print("Information", infoData)
+    return render(request, 'mainApp/course.html')
+
+def course_list(request):
+    all_data = CourseInfo.objects.all()
+    info_data = []
+    for data in all_data:
+        place_json = {
+            'id': data.id,
+            'title': data.title,
+            'students': [student.name for student in data.students.all()]
+        }
+        info_data.append(place_json)
+    print("Info Data ", info_data)
+    return render(request, 'mainApp/courseList.html', {'info_data': info_data})
+
+# views.py
+
+
+# views.py
+
+def student_view(request):
+    all_students = Student.objects.all()
+    info_data = []
+
+    for student in all_students:
+        student_info = {}
+        student_info['id'] = student.id
+        student_info['name'] = student.name
+        student_info['courses'] = [course.title for course in student.courseinfo_set.all()]
+        info_data.append(student_info)
+    print("Student", info_data)
+    return render(request, 'mainApp/student.html', {'info_data': info_data})
+
+
